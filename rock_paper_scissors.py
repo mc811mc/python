210f887c12304f38f02d63f83c701945e43fc60f
Minho Cho <mc811mc@gmail.com>
2020-09-09 23:46:22 +0900

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,30 +6,28 @@ com_num = randint(0,2)
 
 com_sel = computer[com_num]
 
-print(com_sel)
-
 player = input('Rock, paper, scissors GO! ')
 player = player.lower()
 
 if player == 'rock' or 'paper' or 'scissors':    
     #win
     if player == 'rock' and com_sel == 'scissors':
-        print('You win!', player.title(), 'beats', com_sel, '!')
+        print('You win! ', player.title(), ' beats ', com_sel, '!', sep ='')
     if player == "paper" and com_sel == "rock":
-        print('You win!', player.title(), 'beats', com_sel, '!')
+        print('You win! ', player.title(), ' beats ', com_sel, '!', sep ='')
     if player == 'scissors' and com_sel == 'paper':
-        print('You win!', player.title(), 'beats', com_sel, '!')
+        print('You win! ', player.title(), ' beats ', com_sel, '!', sep ='')
 
     #draw
     if player == com_sel:
         print('It\'s a draw!')
-
+        
     #lose
     if player == 'rock' and com_sel == 'paper':
-        print('You lose.', com_sel.title(), "beats", player)
+        print('You lose.', com_sel.title(), "beats", player, '!', sep = '')
     if player == 'paper' and com_sel == 'scissors':
-        print('You lose.', com_sel.title(), "beats", player)
+        print('You lose.', com_sel.title(), "beats", player, '!', sep = '')
     if player == 'scissors' and com_sel == 'rock':
-        print('You lose.', com_sel.title(), "beats", player)
+        print('You lose.', com_sel.title(), "beats", player, '!', ssep = '')
 else:
     print("That is not how you play rock, paper, scissors!")
